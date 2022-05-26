a6f03d1da91d8b48dba77facfabbdc521cc01ae0
kawsar-netizen <60083776+kawsar-netizen@users.noreply.github.com>
2022-05-26 10:43:31 +0600

    Delete web.blade.php

--- web.blade.php
+++
@@ -1,8 +0,0 @@
-
-############################################ Corporate Bill Report by Kawsar ####################################
-Route::group(['prefix'=>'report/corporate/bill','as'=> 'corporate.report.'],function(){
-    Route::get('index',[CorporateBillReportController::class,'index'])->name('bill');
-    Route::get('corporate/bill/report',[CorporateBillReportController::class,'bill_show'])->name('bill_show');
-});
-
-############################################ Corporate Bill Report ####################################
